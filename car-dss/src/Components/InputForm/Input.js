3bb8d6c69b1e3048ae4a04cc1cd5d00aa5aaf845
Aryan Ved <aryanved2002@gmail.com>
2024-07-09 00:19:42 -0400

    connected test API to button and updated front-end

--- car-dss/src/Components/InputForm/Input.js
+++ car-dss/src/Components/InputForm/Input.js
@@ -16,6 +16,9 @@ function Input() {
   const [selectedCylinders, setselectedCylinders] = React.useState([])
   const [selectedBudget, setselectedBudget] = React.useState([])
   const [selectedCarsize, setselectedCarsize] = React.useState([])
+  const [selectedMake, setselectedMake] = React.useState([])
+  const [selectedYear, setselectedYear] = React.useState([])
+  const [selectedTrans, setselectedTrans] = React.useState([])
   const [cityMpg, setCityMpg] = React.useState()
   const [hwyMpg, setHwyMpg] = React.useState()
 
@@ -24,8 +27,25 @@ function Input() {
   const bodyStyles = ["convertible", "hatchback", "sedan", "wagon"]
   const drivetrain = ["fwd", "rwd", "4wd"]
   const cylinders = ["two", "three", "four", "five", "six", "eight"]
-  const budgets = ["5000-10000", "10000-15000", "15000-20000", "20000-25000", "25000-30000", "30000+"]
+  const budgets = ["$5000-$10000", "$10000-$15000", "$15000-$20000", "$20000-$25000", "$25000-$30000", "$30000+"]
   const sizes = ["Smaller", "Medium", "Larger"]
+  const make = [
+    "Acura", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Bugatti", 
+    "Buick", "Cadillac", "Chevrolet", "Chrysler", "Dodge", "Ferrari", "FIAT", 
+    "Ford", "Genesis", "GMC", "Honda", "HUMMER", "Hyundai", "Infiniti", "Kia", 
+    "Lamborghini", "Land Rover", "Lexus", "Lincoln", "Lotus", "Maserati", 
+    "Maybach", "Mazda", "McLaren", "Mercedes-Benz", "Mitsubishi", "Nissan", 
+    "Oldsmobile", "Plymouth", "Pontiac", "Porsche", "Rolls-Royce", "Saab", 
+    "Scion", "Spyker", "Subaru", "Suzuki", "Tesla", "Toyota", "Volkswagen", "Volvo"
+];  
+    const year = [
+  "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999",
+  "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009",
+  "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"
+];  
+    const trans = ["Automatic", "Manual"]
+
+
 
   const callApiTest = async () => {
 
@@ -71,53 +91,68 @@ function Input() {
                 What features do you want in your ride? 
               </Typography> 
             <Grid container direction="row" justifyContent="center">
-            <Grid item xs={6}>
+            <Grid item xs={4}>
               <div>
               <Multiselect label="Number of Doors" options={doors} handle={setSelectedDoors} selected={selectedDoors}></Multiselect> 
               </div>              
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={4}>
               <div>
               <Multiselect label="Fuel Type" options={fuels} handle={setselectedFuel} selected={selectedFuel}></Multiselect>
               </div>              
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={4}>
               <div>
               <Multiselect label="Car Body Styles" options={bodyStyles} handle={setselectedBody} selected={selectedBody}></Multiselect>
               </div>              
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={4}>
               <div>
               <Multiselect label="Drivetrain Type" options={drivetrain} handle={setselectedDrivetrain} selected={selectedDrivetrain}></Multiselect>
               </div>              
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={4}>
               <div>
               <Multiselect label="Number of Cylinders" options={cylinders}handle={setselectedCylinders} selected={selectedCylinders}></Multiselect>
               </div>              
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={4}>
               <div>
               <Multiselect label="Budget" options={budgets} handle={setselectedBudget} selected={selectedBudget}></Multiselect>
               </div>              
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={4}>
               <div>
               <Multiselect label="Car Size" options={sizes} handle={setselectedCarsize} selected={selectedCarsize}></Multiselect>
               </div>              
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={4}>
+              <div>
+              <Multiselect label="Year" options={year} handle={setselectedYear} selected={selectedYear}></Multiselect>
+              </div>              
+              </Grid>
+              <Grid item xs={4}>
+              <div>
+              <Multiselect label="Make" options={make} handle={setselectedMake} selected={selectedMake}></Multiselect>
+              </div>              
+              </Grid>
+              <Grid item xs={4}>
+              <div>
+              <Multiselect label="Transmission" options={trans} handle={setselectedTrans} selected={selectedTrans}></Multiselect>
+              </div>              
+              </Grid>
+              <Grid item xs={4}>
               <div>
               <IntegerInput label="City MPG" handle={setCityMpg} selected={cityMpg}></IntegerInput>
               </div>              
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={4}>
               <div>
               <IntegerInput label="Highway MPG" handle={setHwyMpg} selected={hwyMpg}></IntegerInput>
               </div>              
               </Grid>
             </Grid>
-            <Button variant='contained' style={{backgroundColor: '#4169e1', color: 'white', width: '40vh', height: '5vh', marginBlock: '4vh'}}>Submit</Button>
+            <Button variant='contained' onClick={() => {callApiTest()}} style={{backgroundColor: '#4169e1', color: 'white', width: '40vh', height: '5vh', marginBlock: '4vh'}}>Submit</Button>
 
             </Card>
             </div>
